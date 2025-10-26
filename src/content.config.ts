b3e0f0c29bc56148d5d6289fca676696155be6db
Mario <me@mabreumx.com>
2025-10-25 20:10:55 -0600

    feat(content.config.ts): añadir configuración de colección para blogs en Astro

---
+++ src/content.config.ts
@@ -0,0 +1,20 @@
+import { defineCollection, z } from "astro:content";
+import { glob, file } from "astro/loaders";
+
+const blog = defineCollection({
+  loader: glob({
+    pattern: "**/*.md",
+    base: "./src/data/blog",
+  }),
+  schema: z.object({
+    title: z.string(),
+    authors: z.array(z.string()).optional(),
+    excerpt: z.string().optional(),
+    category: z.string().optional(),
+    image: z.string().optional(),
+  }),
+});
+
+export const collections = {
+  blog,
+};
