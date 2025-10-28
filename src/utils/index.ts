27cf3c588f2a82dfbb19e55e7bf212ec4bd281bd
Mario <31197372+Max131@users.noreply.github.com>
2025-10-28 15:05:45 -0600

    feat(layouts): añadir propiedad classes al componente Default.astro feat(pages): implementar formato de fecha en las páginas de post style(pages): agregar clase post-page al layout de post style(pages): agregar clase home-page al layout de inicio style(global.css): mejorar estilo de post-excerpt y añadir post-link-back chore(utils): crear archivo utils con función getFormatedDate

---
+++ src/utils/index.ts
@@ -0,0 +1,19 @@
+const MONTHS = {
+  "01": "Enero",
+  "02": "Febrero",
+  "03": "Marzo",
+  "04": "Abril",
+  "05": "Mayo",
+  "06": "Junio",
+  "07": "Julio",
+  "08": "Agosto",
+  "09": "Septiembre",
+  "10": "Octubre",
+  "11": "Noviembre",
+  "12": "Diciembre",
+} as const;
+
+export const getFormatedDate = (dateString: string): string => {
+  const [year, month, day] = dateString.split("-");
+  return `${MONTHS[month as keyof typeof MONTHS]} ${day}, ${year}`;
+};
